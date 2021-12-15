7a4649a199faea7f90888101b1a85b5c4946ade4
ElvisBoasman <Elvisboasman@gmail.com>
2021-12-15 13:49:55 -0400

    Add project files.

---
+++ WebApiAutores/Controllers/AutoresControllers.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresControllers : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public AutoresControllers(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+
+            return await context.Autores.Include(x=>x.Libros).ToListAsync();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();   
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] //api/autores/1 o 2 etc....
+
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+
+            if(autor.Id != id)
+            {
+
+                return BadRequest("El id del autor no coincide con el id de la url");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Borrar(int id)
+        {
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id =  id});
+            await context.SaveChangesAsync();
+            return Ok();
+
+           
+        }
+
+    }
+}
